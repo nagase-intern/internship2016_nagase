9c01129d5a62f121cbd1e4e1d6d52430c350ba44
nagasef <nagasefmons@gmail.com>
2016-02-05 05:58:24 +0000

    2/5_1500_trip

--- sinatora_test.rb
+++ sinatora_test.rb
@@ -27,7 +27,14 @@ get '/main/' do
 end
 
 post '/new/' do
-    ActiveRecord::Base.connection.execute("INSERT INTO bbs(name,maintext) VALUES('#{params[:name]}','#{params[:maintext]}');")
+    name=params[:name]
+    trip=nil
+    tripstart=params[:name].rindex("#")
+    if tripstart != nil
+        trip=tripcreate(name,tripstart)
+        name=name[0..tripstart-1]
+    end
+    ActiveRecord::Base.connection.execute("INSERT INTO bbs(name,maintext,trip) VALUES('#{name}','#{params[:maintext]}','#{trip}');")
     redirect '/main/'
 end
 
@@ -36,3 +43,12 @@ post '/delete/' do
     redirect '/main/'
 end
 
+def tripcreate(str,n)
+    trip=str[n+1..-1]
+    key= (trip + "H.").slice(1, 2)
+    key = key.gsub(/[^\.-z]/, ".")
+    key = key.tr(":;<=>?@[\\]^_`", "ABCDEFGabcdef")
+    trip = trip.crypt(key).slice(-10, 10);
+    trip="◆"+trip
+    return trip
+end
